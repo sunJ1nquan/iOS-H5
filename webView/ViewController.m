7c5d1827b6864d973735c4e1f01b1e2b78f91719
sunJ1nquan <18518634525@163.com>
2016-02-02 15:42:53 +0800

    iOS-H5
    iOS原生与H5交互

--- webView/ViewController.m
+++ webView/ViewController.m
@@ -1,14 +1,17 @@
 //
 //  ViewController.m
-//  webView
+//  JavaScriptAndObjectiveC
 //
-//  Created by sjq on 16/1/20.
-//  Copyright © 2016年 SJQ. All rights reserved.
+//  Created by huangyibiao on 15/10/13.
+//  Copyright © 2015年 huangyibiao. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) JSContext *jsContext;
 
 @end
 
@@ -16,12 +19,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:self.webView];
+    
+    // 一个JSContext对象，就类似于Js中的window，只需要创建一次即可。
+    self.jsContext = [[JSContext alloc] init];
+    
+    // jscontext可以直接执行JS代码。
+    [self.jsContext evaluateScript:@"var num = 10"];
+    [self.jsContext evaluateScript:@"var squareFunc = function(value) { return value * 2 }"];
+    // 计算正方形的面积
+    JSValue *square = [self.jsContext evaluateScript:@"squareFunc(num)"];
+    
+    // 也可以通过下标的方式获取到方法
+    JSValue *squareFunc = self.jsContext[@"squareFunc"];
+    JSValue *value = [squareFunc callWithArguments:@[@"20"]];
+    NSLog(@"11111%@", square.toNumber);
+    NSLog(@"111111111%@", value.toNumber);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIWebView *)webView {
+    if (_webView == nil) {
+        _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+        _webView.scalesPageToFit = YES;
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"test" withExtension:@"html"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [_webView loadRequest:request];
+        _webView.delegate = self;
+    }
+    
+    return _webView;
+}
+
+#pragma mark - UIWebViewDelegate
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    // 通过模型调用方法，这种方式更好些。
+    HYBJsObjCModel *model  = [[HYBJsObjCModel alloc] init];
+    self.jsContext[@"OCModel"] = model;
+    model.jsContext = self.jsContext;
+    model.webView = self.webView;
+    
+    self.jsContext.exceptionHandler = ^(JSContext *context, JSValue *exceptionValue) {
+        context.exception = exceptionValue;
+        NSLog(@"异常信息：%@", exceptionValue);
+    };
+    
+    
+    _jsContext[@"activityList"] = ^(NSDictionary *param) {
+        NSLog(@"111111%@", param);
+        
+    };
+    
 }
 
 @end
